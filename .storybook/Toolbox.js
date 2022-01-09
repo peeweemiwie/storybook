aa52dfd2c058f58d9adcd617d899c46a5538c09d
peeweemiwie <miwayagi@gmail.com>
2022-01-09 12:37:41 -0500

    Add brandImage url

--- .storybook/Toolbox.js
+++ .storybook/Toolbox.js
@@ -3,4 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
 	base: 'light',
 	brandTitle: 'Toolbox',
+	brandImage:
+		'https://raw.githubusercontent.com/peeweemiwie/storybook/icon/public/img/logo-toolbox.svg',
 });
