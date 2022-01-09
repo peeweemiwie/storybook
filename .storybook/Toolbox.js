8163a1cdddf2250f30adc800fdee8b4e1b388412
peeweemiwie <miwayagi@gmail.com>
2022-01-09 13:12:51 -0500

    Add theming. Change logo from svg to png

--- .storybook/Toolbox.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-	base: 'light',
-	brandTitle: 'Toolbox',
-	brandImage:
-		'https://raw.githubusercontent.com/peeweemiwie/storybook/icon/public/img/logo-toolbox.svg',
-});
