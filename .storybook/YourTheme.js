ae5f1871edc592900cd5c044a7fe16a368cd7605
peeweemiwie <miwayagi@gmail.com>
2022-01-10 12:12:47 -0500

    remove theme

--- .storybook/YourTheme.js
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming';
-import logo from 'https://eloquent-montalcini-93e218.netlify.app/img/logo-toolbox.svg';
-
-export default create({
-	base: 'light',
-	brandTitle: 'Toolbox',
-	brandUrl: 'https://peeweemiwie.com/',
-	brandImage: logo,
-});
