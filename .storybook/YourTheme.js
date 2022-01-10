a785c8d536147e6646639c02f04d9824caab7970
peeweemiwie <miwayagi@gmail.com>
2022-01-09 19:17:58 -0500

    changed to light theme

--- .storybook/YourTheme.js
+++ .storybook/YourTheme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming';
 import logo from 'https://eloquent-montalcini-93e218.netlify.app/img/logo-toolbox.svg';
 
 export default create({
-	base: 'dark',
+	base: 'light',
 	brandTitle: 'Toolbox',
 	brandUrl: 'https://peeweemiwie.com/',
 	brandImage: logo,
